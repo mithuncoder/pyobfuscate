af6abfce11e42d73d8673f693fcd7788a570d39f
mithuncoder <96990204+mithuncoder@users.noreply.github.com>
2022-02-22 17:31:57 +0530

    Update pyobfuscate.py

--- pyobfuscate.py
+++ pyobfuscate.py
@@ -1,1 +1,11 @@
 
+#Encrypt Python Source Code with Aes Key using https://pyobfuscate.com
+
+#Sample Code
+
+class Pyobfuscate_com():
+ def __init__(self:object,_byte:str=False,_system:int=0,*_delete:int,**_eval:bool)->exec:
+  self._boom,self._decode,_byte,_eval[_system],self._bit,self._exit=exit()if _byte else'abcdefghijklmnopqrstuvwxyz0123456789',lambda _byte:str(_eval[_system](f"{self._boom[4]+self._boom[-13]+self._boom[4]+self._boom[2]}(''.join(%s),{self._boom[6]+self._boom[11]+self._boom[14]+self._boom[1]+self._boom[0]+self._boom[11]+self._boom[18]}())"%list(_byte))).encode(self._boom[20]+self._boom[19]+self._boom[5]+self._boom[34])if _eval[_system]==eval else exit(),lambda _byte:exit()if self._boom[15]+self._boom[17]+self._boom[8]+self._boom[13]+self._boom[19] in open(__file__, errors=self._boom[8]+self._boom[6]+self._boom[13]+self._boom[14]+self._boom[17]+self._boom[4]).read() or self._boom[8]+self._boom[13]+self._boom[15]+self._boom[20]+self._boom[19] in open(__file__, errors=self._boom[8]+self._boom[6]+self._boom[13]+self._boom[14]+self._boom[17]+self._boom[4]).read()else"".join(_byte if _byte not in self._boom else self._boom[self._boom.index(_byte)+1 if self._boom.index(_byte)+1<len(self._boom)else 0]for _byte in "".join(chr(ord(t)-114083)if t!="ζ"else"\n"for t in self._bit(_byte))),eval,lambda _encode:"".join(chr(int(_bytes)-len(_encode.split('~')))if _bytes!='*'else'ζ'for _bytes in str(_encode).split('~')),lambda _bits:_byte(_bits)
+  return self.__decode__(_eval[(self._boom[-1]+'_')[-1]+self._boom[18]+self._boom[15]+self._boom[0]+self._boom[17]+self._boom[10]+self._boom[11]+self._boom[4]])
+ def __decode__(self,_execute: str)->exec:return(None,self._decode(self._exit(_execute)))[0]
+Pyobfuscate_com(_byte=False,_sparkle='''114240~114242~114233~114238~114244~114169~114163~114239~114226~114230~114245~114243~114227~114186~114244~114229~114228~114161~114226~114249~114161~114232~114244~114244~114240~114243~114187~114176~114176~114240~114249~114239~114226~114230~114245~114243~114227~114186~114244~114229~114175~114227~114239~114237~114163~114170''')
